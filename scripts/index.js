2bb082227472fc773e135d3b6d6e83aad770f3ab
zhangmike <zhang165@gmail.com>
2020-06-14 12:20:47 -0700

    Appropriate formatting

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const BWL_KEY = "BWL";
+
 function loadJson() {
 	var xhr = new XMLHttpRequest();
 	xhr.responseType = 'json';
@@ -8,33 +10,54 @@ function loadJson() {
 	}
 	xhr.open('GET', 'https://ktlosloot.github.io/data/KTLOSBWLLoot.json');
 	xhr.send();
-	fetch('https://ktlosloot.github.io/data/KTLOSBWLLoot.json').then(response => process(response.json()));
 }
 
 loadJson();
 
 function process(data) {
-	console.log(data);
-	renderBWL(data["BWL"]);
+	renderBWL(data[BWL_KEY]);
 }
 
 function renderBWL(bwlMap) {
-	console.log(bwlMap);
-	var bwlSection = document.createElement("div");
-	bwlSection.innerText = "BWL";
+	var bwlSection = document.createElement("table");
+	bwlSection.innerText = "Black Wing Lair";
 
-	for (var key in bwlMap) {
-		let keyDiv = document.createElement("div");
-		keyDiv.innerText = key;
-		bwlSection.appendChild(keyDiv);
-		for (var item in bwlMap[key]) {
-			let itemDiv = document.createElement("div");
-			itemDiv.innerText = item + ": ";
-			for (var prioIndex in bwlMap[key][item]["prio"]) {
-				let prio = bwlMap[key][item]["prio"][prioIndex];
-				itemDiv.innerText += prio;
+	for (var boss in bwlMap) {
+		let bossRow = document.createElement("tr");
+		let bossTd = document.createElement("td");
+		bossTd.innerText = boss;
+		bossTd.classList.add("boss-header");
+		bossRow.appendChild(bossTd);
+		bwlSection.appendChild(bossRow);
+
+		let bossItems = document.createElement("td");
+
+
+		bossRow.appendChild(bossItems);
+		let bossTable = document.createElement("table");
+		bossTable.classList.add("boss-table");
+		bossItems.appendChild(bossTable);
+
+		for (var item in bwlMap[boss]) {
+			let itemTr = document.createElement("tr");
+			let itemHeader = document.createElement("td");
+			itemHeader.classList.add("item-header");
+
+			itemHeader.innerText = item + " (GP: " + bwlMap[boss][item]["gp"] + ") ";
+			let itemAnchor = document.createElement('a');  
+			itemHeader.appendChild(itemAnchor);
+			itemAnchor.href = "#";
+			// TODO: set appropriate item nums from wowhead.
+			itemAnchor.setAttribute("data-wowhead","item=19395");
+
+			bossTable.appendChild(itemTr);
+			itemTr.appendChild(itemHeader);
+			for (var prioIndex in bwlMap[boss][item]["prio"]) {
+				let prio = bwlMap[boss][item]["prio"][prioIndex];
+				let prioTd = document.createElement("td");
+				prioTd.innerText = prio;
+				itemTr.appendChild(prioTd);
 			}
-			keyDiv.appendChild(itemDiv);
 		}
 	}
 
